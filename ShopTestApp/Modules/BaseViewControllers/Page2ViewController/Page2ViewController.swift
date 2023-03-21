1dd18972c6a2135d2d115401f8c9c1e2936a7db6
kudr1kk22 <kudr1kk22@gmail.com>
2023-03-21 21:00:56 +0300

    add compositionlayout for cells

--- ShopTestApp/Modules/BaseViewControllers/Page2ViewController/Page2ViewController.swift
+++ ShopTestApp/Modules/BaseViewControllers/Page2ViewController/Page2ViewController.swift
@@ -234,6 +234,31 @@ extension Page2ViewController {
       
       return section
 
+    case 2:
+      // Item
+      let item = NSCollectionLayoutItem(
+        layoutSize: NSCollectionLayoutSize(
+          widthDimension: .fractionalWidth(1.0),
+          heightDimension: .fractionalHeight(1.0)
+        )
+      )
+
+      item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+
+      let group = NSCollectionLayoutGroup.vertical(
+        layoutSize: NSCollectionLayoutSize(
+          widthDimension: .fractionalWidth(1.0),
+          heightDimension: .absolute(390)
+        ),
+        subitem: item,
+        count: 1
+      )
+
+
+      let section = NSCollectionLayoutSection(group: group)
+      section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 0, bottom: 0, trailing: 0)
+      section.boundarySupplementaryItems = supplementaryViews
+      return section
     default:
       // Item
       let item = NSCollectionLayoutItem(
